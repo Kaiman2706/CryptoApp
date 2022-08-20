d8533f80c6bbd89d0332ec53c7e73b2eaba1b333
Kaiman de Mello Cunha <kaimanmello123@yahoo.com>
2022-08-20 14:22:14 -0300

    commit test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {AngularFireModule} from '@angular/fire/compat';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,7 @@ import { Test2Component } from './test2/test2.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './main/main.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import { BrowserModule } from '@angular/platform-browser';
     BrowserModule,
     MatInputModule,
     FormsModule,
-    MatIconModule
+    MatIconModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [AppComponent],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,16 @@ export const environment = {
   //LOCAL
   production: false,
   name: "Local",
-  severUrl: 'https://localhost:7079/api/Crypto/GetCrypto'
+  severUrl: 'https://localhost:7079/api/Crypto/GetCrypto',
+  firebase : {
+    apiKey: "AIzaSyAslPnsjD6jUMYDoFxj1iSHTfmabZltXIE",
+    authDomain: "bancoroleplayinggamemello.firebaseapp.com",
+    projectId: "bancoroleplayinggamemello",
+    storageBucket: "bancoroleplayinggamemello.appspot.com",
+    messagingSenderId: "505012578766",
+    appId: "1:505012578766:web:88bcff553157042943e39f",
+    measurementId: "G-LERT091MDY"
+  },
 };
 
 /*
